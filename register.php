18e2e4e62ee538b1e2cf25b5ebba362163ea8284
LeoZX12 <124744510+LeoZX12@users.noreply.github.com>
2023-04-28 23:37:09 +0800

    login and register redesign

--- register.php
+++ register.php
@@ -18,27 +18,17 @@ $view = new view;
 
  </head>
  <body>
-         <nav class="navbar navbar-dark bg-white shadow-sm slide-in-left">
-           <a class="navbar-brand" href="https://malolos.ceu.edu.ph/">
-             <img src="resource/img/logo.jpg" height="70" class="d-inline-block align-top"
-               alt="mdb logo"><h3 class="ib">
-           </a>
-           <a href="exportTableAdmin.php"><i class="fas fa-table ceucolor"></i></a>
-           <a href="statsAdmin.php"><i class="fas fa-chart-line ceucolor"></i></a>
-           <a href="userVerificationAdmin.php"><i class="fas fa-user-plus ceucolor"></i></a>
-           <a href="verificationAdmin.php"><i class="fas fa-user-graduate ceucolor"></i></a>
-           <a href="viewAlumniAdmin.php"><i class="fa fa-graduation-cap ceucolor"></i></a>
-           <a href="nTransactionAdmin.php"><i class="fas fa-file-upload ceucolor"></i></a>
-           <a href="view_pending_requests.php"><i class="fas fa-home ceucolor"></i></a>
-           <a href="https:/www.facebook.com/theCEUofficial/"><i class="fab fa-facebook-f ceucolor"></i></a>
-           <a href="https://www.instagram.com/ceuofficial/"><i class="fab fa-instagram ceucolor"></i></a>
-           <a href="https://twitter.com/ceumalolos"><i class="fab fa-twitter ceucolor"></i></a>
-         </nav>
+        <nav class="navbar shadow-sm slide-in-left">
+          <a class="navbar-brand " href="https://malolos.ceu.edu.ph/">
+            <img src="resource/img/cmuLGO.png" height="100" class="d-inline-block align-top"
+              alt="mdb logo"><h3 class="ib">
+          </a>
+        </nav>
 
          <div class="container mt-4 puff-in-center">
              <div class="row">
                  <div class="col-12">
-                     <h1 class="text-center">Register New Student Records Assistant</h1>
+                     <h1 class="text-center text-white">Register New Student Records Assistant</h1>
                  </div>
             </div>
             <?php
@@ -71,12 +61,12 @@ $view = new view;
                                  <label for = "fullName" class=""> Full Name</label>
                                  <input class="form-control"  type = "text" name="fullName" id="fullName" value ="<?php echo input::get('fullName');?>"/required>
                                 </div>
-                                <div class="form-group col-4">
+                                <!-- <div class="form-group col-4">
                                   <label for="College" >College/s to handle</label>
                                       <select id="College" name="College[]" class="selectpicker form-control" data-live-search="true" multiple required>
-                                        <?php $view->collegeSP2();?>
+                                        <?php //$view->collegeSP2();?>
                                       </select>
-                                </div>
+                                </div> -->
                                 <div class="form-group col-4">
                                  <label for = "email" class=""> Email Address</label>
                                  <input class="form-control"  type = "text" name="email" id="email" value ="<?php echo input::get('email');?>"/required>
@@ -90,7 +80,7 @@ $view = new view;
                                 <div class="form-group col-7">
                                     <label  >&nbsp;</label>
                                 <input type="hidden" name ="Token" value="<?php echo Token::generate();?>" />
-                                 <input type="submit" value="Register New SRA" class=" form-control btn btn-primary" />
+                                 <input type="submit" value="Register New SRA" class=" form-control btn-reg" />
                                 </div>
                             </div>
                         </td>
@@ -103,10 +93,10 @@ $view = new view;
    <div class="container text-center">
        <div class="row">
            <div class="col col-sm-5 text-left">
-               <small>Copyright &copy;Centro Escolar University     Office of the Registrar 2019</small>
+               <small>Copyright &copy;Centro Muscular University     Office of the Registrar 2023</small>
            </div>
            <div class="col text-right">
-               <small>Created by: Reymart Bolasoc, Amelia Valencia , James Mangalile, Kenneth De Leon , Pamela Reyes , Ellen Mijares</small>
+               <small>Created by: Lyzza Abig, Alcedrick Garcia, Joanna Policarpio, Leonard Bruno, Jannah Pescadera</small>
            </div>
        </div>
    </div>
